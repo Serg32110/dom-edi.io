e8c03ebd9ccc108db0533e3a9421a6be68cd90ea
Serg32110 <157228383+Serg32110@users.noreply.github.com>
2024-01-25 13:55:43 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    
 
     // Адрес, на который нужно отправить письмо
     $to = "s.ryabinin123@gmail.com";
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Текст письма
     $message = "Имя: " . $name . "\n"
         . "Email: " . $email . "\n"
-        . "Номер телефона: " . $phone;
+        
 
     // Отправка письма
     if (mail($to, $subject, $message)) {
